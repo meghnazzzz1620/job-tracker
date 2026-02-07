e218c6b785437af7cebb37ab5f78e353f06e384f
Meghna1620 <mpradhan1605@gmail.com>
2026-02-07 14:22:09 +0530

    Add global CORS config for Vercel frontend

--- backend/backend/src/main/java/com/jobtracker/backend/config/WebConfig.java
+++ backend/backend/src/main/java/com/jobtracker/backend/config/WebConfig.java
@@ -1,7 +1,8 @@
 package com.jobtracker.backend.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -9,8 +10,11 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowedHeaders("*");
+                .allowedOrigins(
+                        "http://localhost:5173",
+                        "https://job-tracker-frontend-ashen.vercel.app/")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
